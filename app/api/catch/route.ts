eb76016751f3cb8ce07be0b8bb1ad36bad10f1d5
1987566643 <51523972+1987566643@users.noreply.github.com>
2025-08-19 23:19:21 +0800

    Update route.ts

--- app/api/catch/route.ts
+++ app/api/catch/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { getSession } from '../../lib/auth';
-import { sql } from '../../lib/db';
+import { getSession } from '../../../lib/auth';
+import { sql } from '../../../lib/db';
 
 export async function POST(req: Request) {
   const session = await getSession();
