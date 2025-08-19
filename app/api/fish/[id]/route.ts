5b758c197e6adb6a0c4b99e8eecd9a40c694e5cf
1987566643 <51523972+1987566643@users.noreply.github.com>
2025-08-19 23:20:54 +0800

    Update route.ts

--- app/api/fish/[id]/route.ts
+++ app/api/fish/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { getSession } from '../../../lib/auth';
-import { sql } from '../../../lib/db';
+import { getSession } from '../../../../lib/auth';
+import { sql } from '../../../../lib/db';
 
 type Params = { params: { id: string } };
 
